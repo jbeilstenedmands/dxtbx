a170df9a171535dc7df67da8c94537eee6016be2
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2022-10-18 12:25:05 +0100

    Experimental change to give a scan and gonio for ssx processing

--- src/dxtbx/format/FormatHDF5SaclaRayonix.py
+++ src/dxtbx/format/FormatHDF5SaclaRayonix.py
@@ -61,6 +61,7 @@ class FormatHDF5SaclaRayonix(FormatHDF5, FormatStill):
         self._images = sorted([tag for tag in h5_handle if tag.startswith("tag-")])
         self.tag = self._images[self.index]
         h5_handle.close()
+        self._setup_gonio_and_scan()
 
     def read_metadata(self):
         h5_handle = h5py.File(self.image_filename, "r")
@@ -137,6 +138,34 @@ class FormatHDF5SaclaRayonix(FormatHDF5, FormatStill):
             self._beam_instance = self._beam()
 
         return self._beam_instance
+
+    def _setup_gonio_and_scan(self):
+        from dxtbx.model.scan import Scan
+
+        num_images = self.get_num_images()
+        image_range = (1, num_images)
+        oscillation = (0.0, 0)
+
+        exposure_time = flex.double(num_images, 0)
+        epochs = flex.double(num_images, 0)
+
+        # Construct the model
+        self._scan_model = Scan(image_range, oscillation, exposure_time, epochs)
+        self._goniometer_instance = self._goniometer_factory.known_axis((0, 1, 0))
+
+    def _scan(self):
+        return self._scan_model
+
+    def get_scan(self, index=None):
+        if index is None:
+            return self._scan()
+        scan = self._scan()
+        if scan is not None:
+            return scan[index]
+        return scan
+
+    def get_goniometer(self, index=None):
+        return self._goniometer_instance
 
 
 if __name__ == "__main__":
