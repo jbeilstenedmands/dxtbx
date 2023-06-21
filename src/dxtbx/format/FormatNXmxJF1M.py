190035ff18321e307adfa7a324c181959e386d4e
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2023-06-21 16:04:56 +0100

    Set image size

--- src/dxtbx/format/FormatNXmxJF1M.py
+++ src/dxtbx/format/FormatNXmxJF1M.py
@@ -1,14 +1,13 @@
-from dxtbx.format.FormatNXmxDLS import (
-    FormatNXmxDLS,
-)
+from __future__ import annotations
 
+from dxtbx.format.FormatNXmxDLS import FormatNXmxDLS
 
 trusted = (-5, 1e6)
 
+
 class FormatNXmxJF1M(FormatNXmxDLS):
-
-
     def _start(self):
         super()._start()
         for panel in self._detector_model:
             panel.set_trusted_range((trusted[0], trusted[1]))
+            panel.set_image_size((1066, 1030))
