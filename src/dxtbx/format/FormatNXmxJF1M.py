0738efe28a3181e62a74f059a46ea072899fd21f
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2023-06-22 11:18:37 +0100

    Add some defaults and better understand method

--- src/dxtbx/format/FormatNXmxJF1M.py
+++ src/dxtbx/format/FormatNXmxJF1M.py
@@ -1,13 +1,51 @@
 from __future__ import annotations
 
-from dxtbx.format.FormatNXmxDLS import FormatNXmxDLS
+import h5py
+import nxmx
 
-trusted = (-5, 1e6)
+from dxtbx.format.FormatNXmxDLS import FormatNXmxDLS
+from dxtbx.model.detector_helpers import (
+    set_detector_distance,
+    set_fast_slow_beam_centre_mm,
+)
+
+# some sensible defaults
+TRUSTED = (-5, 1e6)
+DISTANCE = 116.3
+FAST_SLOW_BEAM_CENTRE = 558, 955
 
 
 class FormatNXmxJF1M(FormatNXmxDLS):
+    @staticmethod
+    def understand(image_file):
+        with h5py.File(image_file) as handle:
+            name = nxmx.h5str(FormatNXmxDLS.get_instrument_name(handle))
+            file_handle = h5py.File(image_file, swmr=False)
+            nxmx_obj = nxmx.NXmx(file_handle)
+            nxdata = nxmx_obj.entries[0].data[0]
+            if nxdata.signal:
+                data = nxdata[nxdata.signal]
+            else:
+                data = list(nxdata.values())[0]
+            image_size = data.shape[1:]
+            if image_size == (1066, 1030):
+                if name and name.lower() in ("i03", "i04", "i24", "vmxi"):
+                    return True
+        return False
+
     def _start(self):
         super()._start()
         for panel in self._detector_model:
-            panel.set_trusted_range((trusted[0], trusted[1]))
+            panel.set_trusted_range((TRUSTED[0], TRUSTED[1]))
             panel.set_image_size((1066, 1030))
+        set_detector_distance(self._detector_model, DISTANCE)
+        px_size_f, px_size_s = self._detector_model[0].get_pixel_size()
+        fast_slow_beam_centre_mm = (
+            FAST_SLOW_BEAM_CENTRE[0] * px_size_f,
+            FAST_SLOW_BEAM_CENTRE[1] * px_size_s,
+        )
+        beam = self._beam()
+        assert beam is not None
+        set_fast_slow_beam_centre_mm(
+            self._detector_model, beam, fast_slow_beam_centre_mm, panel_id=0
+        )
